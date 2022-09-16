e60e2ca36d56f5c2b139a6006590bfd303629d2e
CosmicOrder <Matrixxturn@yandex.ru>
2022-09-17 01:47:07 +0300

    refactor: move func fetch_coordinates to locations app

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,4 +1,3 @@
-import requests
 from django import forms
 from django.conf import settings
 from django.contrib.auth import authenticate, login
@@ -11,6 +10,7 @@ from geopy import distance
 
 from foodcartapp.models import Product, Restaurant, Order, RestaurantMenuItem
 from locations.models import Location
+from locations.views import fetch_coordinates
 
 
 class Login(forms.Form):
@@ -190,22 +190,3 @@ def view_orders(request):
                   context={
                       'order_items': orders,
                   })
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection'][
-        'featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lat, lon
