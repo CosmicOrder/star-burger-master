02d9a74eac8cd449409d4b0e354b20ba715f7cb4
CosmicOrder <Matrixxturn@yandex.ru>
2022-11-13 00:50:41 +0000

    feat: update deploy bash script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -8,4 +8,15 @@ npm ci --dev
 python manage.py collectstatic --noinput
 python manage.py migrate
 systemctl reload nginx
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header 'X-Rollbar-Access-Token: 6f1b2573f71147ab9d34e6e882ec7563' \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+{
+     "environment": "production",
+     "revision": "'$(git rev-parse HEAD)'"
+}
+'
 echo "Диплой прошёл успешно!"
